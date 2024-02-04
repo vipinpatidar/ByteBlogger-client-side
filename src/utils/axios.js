3759dc3f4c6ce2429a32e4053b7abea7328f5b43
vipin Patidar <vipinpatidar5@gmail.com>
2024-02-04 22:29:05 +0530

    added header to axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,8 +4,31 @@ const BASE_URL = `${import.meta.env.VITE_HOST_URL}/api/`;
 
 axios.defaults.withCredentials = true;
 
+// Create a function to retrieve the token from sessionStorage
+const getToken = () => {
+  const user = sessionStorage.getItem("user");
+  return user ? JSON.parse(user).token : null;
+};
+
+// Create a function to make requests with the Authorization header
+export const token = () => {
+  const token = getToken();
+
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  // Add Authorization header if a token is available
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
 export const makeRequest = axios.create({
   baseURL: BASE_URL,
+  headers: token(),
 });
 
 /*
