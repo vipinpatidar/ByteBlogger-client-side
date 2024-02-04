6bb2dd93034cbef6c909a16c0e5e29e1d87075c1
vipin Patidar <vipinpatidar5@gmail.com>
2024-02-04 18:38:18 +0530

    added backend fixed url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_HOST_UR}/api/`;
+const BASE_URL = `${import.meta.env.VITE_HOST_URL}/api/`;
 
 axios.defaults.withCredentials = true;
 
